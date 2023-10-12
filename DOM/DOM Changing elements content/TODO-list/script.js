518d9bc92e4ed0aa61bda3744c490c384dd88e51
sonicnos <107223123+sonicnos@users.noreply.github.com>
2023-10-12 16:19:29 +0200

    git commit -m "12-10-23"

--- DOM/DOM Changing elements content/TODO-list/script.js
+++ DOM/DOM Changing elements content/TODO-list/script.js
@@ -1,13 +1,43 @@
 const addinput = document.querySelector(".add-todo-input");
 const addBtn = document.querySelector(".btn-add");
-const taskAdded = new Set();
+const todoList = document.querySelector(".todo-list");
+
+let todoListArray = []
+
+
+const deleteTask = (indexArr) => {
+    console.log("works " + indexArr);
+}
 
 const addTaskToList = (input) => {
+    let newItem = document.createElement("li");
+    itemId = todoListArray.length - 1;
+
+    todoList.appendChild(newItem);
+    newItem.appendChild(document.createTextNode(input));
+    createDeleteItemBtn = newItem.appendChild(document.createElement("button"));
+    createDeleteItemBtn.innerText = "Delete " + itemId;
+    createDeleteItemBtn.className = 'dlt-btn-' + itemId;
+    createDeleteItemBtn.setAttribute('id' + "#dlt-btn");
+    selectedDeleteBtn = document.querySelector("#dlt-btn");
+    createDeleteItemButton.setAttribute("onclick", "deleteTask()");
+    newItem.addEventListener ("click", (e) => {
+        console.log(newItem);
+    })
 
 }
 
 addBtn.addEventListener ("click", (e) => {
-    console.log(taskAdded);
-    console.log(addinput.value);
+    let addInputValue = addinput.value
+    let checkTaskExistInTodoList = todoListArray.includes(addInputValue);
+    if(!checkTaskExistInTodoList) {
+        todoListArray.push(addInputValue);
+        addTaskToList(addInputValue);
+    }
 
-})
+
+    console.log(todoListArray);
+    console.log(checkTaskExistInTodoList);
+});
+
+
