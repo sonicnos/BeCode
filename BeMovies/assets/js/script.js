4f7e6753bd60c6c3c14617ceef686ea3f6c12a24
sonicnos <107223123+sonicnos@users.noreply.github.com>
2023-10-24 17:00:09 +0200

    commit -m "24-10-23"

--- BeMovies/assets/js/script.js
+++ BeMovies/assets/js/script.js
@@ -1,9 +1,19 @@
 const searchBar = document.getElementById('search-bar');
 const submitBtn = document.getElementById('submit-btn');
 const resultsDisplayValue = document.querySelector('.search-bar-value');
-
+const openBtn = document.querySelector("data-open-modal");
+const closeBtn = document.querySelector("data-close-modal");
+const modal = document.querySelector("data-modal");
+const overlay = document.querySelector("data-overlay");
 
 
 submitBtn.addEventListener ('click', (e) => {
     resultsDisplayValue.innerText = searchBar.value;
-})
+});
+
+openBtn.addEventListener("click", () => {
+    modal.classList.add("open");
+    overlay.classList.add("open");
+});
+
+closeBtn.addEventListener('click', () =>)
