898220fc27167f82184fdc8cc704e0b3b8e82375
sonicnos <107223123+sonicnos@users.noreply.github.com>
2023-11-15 14:51:31 +0100

    -commit -m "15-11-23"

--- nodejs/server/script.js
+++ nodejs/server/script.js
@@ -1,11 +1,35 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   console.log('request made ' + req.url, req.method);
   res.setHeader("Content-Type", 'text/html');
-  res.write('<p>hello</p>');
-  res.write('<p>second balise</p>');
-  res.end();
+
+  let path = './views/';
+  switch(req.url){
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    default:
+      path += '404.html'
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end()
+    }else{
+      res.write(data);
+      res.end();
+    }
+  })
+
+  // res.write('<p>hello</p>');
+  // res.write('<p>second balise</p>');
+  // res.end();
 })
 
 server.listen(3000, 'localhost', () => {
