3dd20ff5aed024c0fa1f07ebafee71b8052eca64
sonicnos <107223123+sonicnos@users.noreply.github.com>
2023-11-29 16:59:24 +0100

    29-11-23

--- nodejs/mongoDB/ninja smoothies/controllers/authController.js
+++ nodejs/mongoDB/ninja smoothies/controllers/authController.js
@@ -1,3 +1,28 @@
+const User = require('../models/User')
+
+//handle errors
+
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email:'', password:''};
+
+    //duplicate errors
+    if(err.code === 11000) {
+        errors.email = "that email is already registered"
+        return errors;
+    }
+
+
+    //validation errors
+    if(err.message.includes('user validation failed')){
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message;
+        });
+    }
+
+    return errors;
+}
+
 module.exports.signup_get = (req, res) => {
     res.render("signup");
 }
@@ -6,10 +31,20 @@ module.exports.login_get = (req, res) => {
     res.render("login");
 }
 
-module.exports.signup_post = (req, res) => {
-    res.render("signup");
+module.exports.signup_post =  async (req, res) => {
+    const {email, password} = req.body;
+    console.log(email, password);
+    try {
+        const user = await User.create({email, password});
+        res.status(201).json(user);
+    }catch(err){
+        const errors = handleErrors(err);
+        res.status(400).json({ errors })
+    }
 }
 
 module.exports.login_post = (req, res) => {
-    res.render("login");
+    // const {email, password} = req.body;
+    // console.log(email, password);
+    res.send("user login");
 }
